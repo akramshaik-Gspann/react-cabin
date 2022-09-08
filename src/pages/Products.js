21947ad891674e2f6ab5bb51519f22801abcd639
Srilekhachanda <srilekha.chanda@gspann.com>
2022-09-08 11:49:42 +0530

    changes

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -2,36 +2,44 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { DataContext } from "../components/Context";
 import "../styles/Products.css";
-
 export class Products extends Component {
   static contextType = DataContext;
+  state = {
+    products : this.context.products
+  }
+  filterData = (e) => {
+      if(e.target.value =="asc"){
+        const products = this.state.products.sort((a,b) => a.capacity - b.capacity);
+        this.setState({products: products})
+      }
+      else{
+        const products = this.state.products.sort((a,b) => b.capacity - a.capacity);
+        this.setState({products: products})
+      }
+  }
   render() {
-    const { products, addCart } = this.context;
-    console.log(products);
+    const { addCart } = this.context;
+    console.log(this.state.products);
     return (
       <div>
         <div>
           <center>
-            <h2>Over 10+ Cabins Instantly Bookable</h2>
+            <h3>Over 10+ Cabins Instantly Bookable</h3>
           </center>
         </div>
-        {/* <div>
-                    SELECT DATE:<input type="datetime-local" className='page1' />
-                    <div className='cabindropdown'> <label for="cars">Choose a cabin:</label>
-                        <select name="cars" id="cars">
-                            <option value="volvo">select_cabin</option>
-                            <option value="volvo">cabin_201to205</option>
-                            <option value="saab">cabin_301to305</option>
-                            <option value="opel">cabin_222</option>
-                            <option value="audi">cabin_333</option>
+        {<div>
+                    <div className='cabindropdown'> <label for="cars">Filter:</label>
+                        <select name="cars" id="cars" onChange={this.filterData} >
+                            <option value="default">Default</option>
+                            <option value="asc">Team size Asc</option>
+                            <option value="desc">Team size Desc</option>
                         </select></div>
-                </div> */}
+                </div>}
         <div id="product">
-          {products.map((product) => (
+          {this.state.products.map((product) => (
             <div className="card" key={product._id}>
               <Link to={`/product/${product._id}`}>
                 <img src={product.src} alt="" />
-
                 <div className="content">
                   <h3>
                     <Link to={`/product/${product._id}`}>{product.title}</Link>
@@ -40,7 +48,7 @@ export class Products extends Component {
                     <p>TeamSize: {product.capacity}</p>
                     <p>{product.Newcabin}</p>
                   </div>
-                  {/*                                     
+                  {/*
                                     <Link to={`/product/${product._id}`}> */}
                   {/* <button onClick={() => addCart(product._id)}>Book Now</button> */}
                   {/* </Link> */}
@@ -53,5 +61,4 @@ export class Products extends Component {
     );
   }
 }
-
 export default Products;
